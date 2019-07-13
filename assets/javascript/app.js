4ad3dbb9addd3dc6aeef25410ffe111e70c3463f
Seth Randell <sethhrandell@gmail.com>
2019-07-12 20:43:04 -0400

    added the logic to generate the times off the data in the database

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -18,26 +18,65 @@
 
 $("#submit-train").on("click", function(event) {
     event.preventDefault();
-    console.log("clciked");
+    // console.log("clciked");
     
 //Get Inputs from add Train Form
   var trainName = $("#trainInput").val().trim();
   var destinationName = ($("#destinationInput").val().trim());
-  var firsttrainTime = parseInt($("#firstInput").val().trim());
+  var firstTrainTime = ($("#firstInput").val().trim());
   var frequencyTime = parseInt($("#frequencyInput").val().trim());
 
 // Log variables
-  console.log(trainName);
-  console.log(destinationName);
-  console.log(firsttrainTime);
-  console.log(frequencyTime);
+//   console.log(trainName);
+//   console.log(destinationName);
+//   console.log(firsttrainTime);
+//   console.log(frequencyTime);
 
 // Set values in Firebase
 database.ref().push({
     trainName: trainName,
     destinationName: destinationName,
-    firsttrainTime: firsttrainTime,
-    frequencyTime: frequencyTime
+    firstTrainTime: firstTrainTime,
+    frequencyTime: frequencyTime,
+    dateAdded: firebase.database.ServerValue.TIMESTAMP
   });
 
-})
+});
+
+  database.ref().on("child_added", function(childSnapshot) {
+
+// Creates variable for the childSnapshot      
+    var snapshotValue = childSnapshot.val();
+// Gets current time
+    var currentTime = moment();
+// Coverts time so it willalways be behind the time entered
+    var convertedTime = moment(snapshotValue.firstTrainTime ,"HH:mm").subtract(1, "years");
+// Tells the diffrence in time in th convertedTime variable
+    var timeDiff = moment().diff(moment(convertedTime), "minutes");
+//Calculates the time remaining
+    var timeRemaining = timeDiff % snapshotValue.frequencyTime;
+// Calculates mins till train
+    var minsTillTrian = snapshotValue.frequencyTime - timeRemaining;
+// Calculates misn away based on the minsTillTrian cvariable
+    var nextTrain = moment().add(minsTillTrian, "minutes");
+
+
+    if(snapshotValue.trainName === "blah"){
+        console.log("Looks like that trian is already there")
+        
+    }else {
+// Create the new row
+    var newRow = $("<tr>").append(
+    $("<td>").text(snapshotValue.trainName),
+    $("<td>").text(snapshotValue.destinationName),
+    $("<td>").text(snapshotValue.frequencyTime),
+    $("<td>").text(moment(nextTrain).format("hh:mm")),
+    $("<td>").text(minsTillTrian)
+    
+   
+  );
+  $("#train-table > tbody").append(newRow);
+
+    }
+    
+  });
